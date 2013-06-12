34a6daf9c50b2f2c23241664da8016691640dd0d
James Nutt <james.srn@gmail.com>
2013-06-12 11:48:30 +0100

    Enhanced prototype

--- Collage.py
+++ Collage.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import os
 from PIL import Image
+import math
 
 outputdir = 'out/'
 
@@ -26,3 +27,36 @@ for imagename in imagelist:
 
 # That mostly works, but with some bizzare aspect ratio issues
 
+# Calculate how much space we need
+maxwidth = 800
+row = 0
+xoffset = 0
+
+for imagename in imagelist:
+	im = Image.open("out/" + imagename)
+	width, height = im.size
+	if xoffset + width > maxwidth:
+		row += 1
+		xoffset = 0
+	else:
+		xoffset += width
+
+requiredheight = math.ceil(row * newheight)
+print "Required height: " + str(requiredheight) 
+
+blank_image = Image.new("RGB", (maxwidth, int(requiredheight)))
+
+row = 0
+xoffset = 0
+
+for imagename in imagelist:
+	im = Image.open("out/" + imagename)
+	width, height = im.size
+	if xoffset + width > maxwidth:
+		row += 1
+		xoffset = 0
+	print "Pasting to: " + str(xoffset) + "," + str(int(row*newheight))
+	blank_image.paste(im, ( xoffset, int(row*newheight) ))
+	xoffset += width
+
+blank_image.save(outputdir + 'collage.jpg', "JPEG")
