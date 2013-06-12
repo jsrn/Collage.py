e6b329e81985eb0293243fe4fc509a43e49896d0
James Nutt <james.srn@gmail.com>
2013-06-12 12:21:15 +0100

    More efficient

--- Collage.py
+++ Collage.py
@@ -28,24 +28,9 @@ for imagename in imagelist:
 	im = im.resize((int(newwidth), int(newheight)), Image.ANTIALIAS)
 	im.save(outputdir + imagename, "JPEG")
 
-# That mostly works, but with some bizzare aspect ratio issues
-
-# Calculate how much space we need
 maxwidth = 800
-row = 0
-xoffset = borderwidth
 
-for imagename in imagelist:
-	im = Image.open("out/" + imagename)
-	width, height = im.size
-	if xoffset + width + borderwidth > maxwidth:
-		row += 1
-		xoffset = 0
-	xoffset += width + borderwidth
-
-requiredheight = math.ceil((row+1) * newheight + (row+1)*borderwidth)
-
-blank_image = Image.new("RGB", (maxwidth, int(requiredheight)), backgroundcolour)
+blank_image = Image.new("RGB", (maxwidth, 200), backgroundcolour)
 
 row = 0
 xoffset = borderwidth
@@ -53,9 +38,17 @@ xoffset = borderwidth
 for imagename in imagelist:
 	im = Image.open("out/" + imagename)
 	width, height = im.size
+
 	if xoffset + width + borderwidth > maxwidth:
 		row += 1
 		xoffset = 0
+
+	requiredheight = int(math.ceil((row+1) * newheight + (row+1)*borderwidth))
+	if blank_image.size[1] < requiredheight:
+		new_blank_image = Image.new("RGB", (maxwidth, requiredheight), backgroundcolour)
+		new_blank_image.paste(blank_image, (0,0));
+		blank_image = new_blank_image
+
 	blank_image.paste(im, ( xoffset + borderwidth, int(row*newheight + (row+1)*borderwidth) ))
 	xoffset += width + borderwidth
 
